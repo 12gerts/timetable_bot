a26fa9abbf8e4815a039981bd3f25435e55f71bb
Gertseva Eva <12gerts@bk.ru>
2022-10-09 22:20:18 +0500

    Delete duplicate files

--- src/Schedule.java
+++
@@ -1,8 +0,0 @@
-public class Schedule {
-    public String getSchedule(int day) {
-        return switch (day) {
-            case 1, 2, 3, 4, 5, 6 -> Report.NO_SCHEDULE; // TODO change Report
-            default -> Report.NO_SCHEDULE;
-        };
-    }
-}
