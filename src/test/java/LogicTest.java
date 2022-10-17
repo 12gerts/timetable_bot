468ef536408ac02ac02f9e3e280cdd0be6ef16aa
Gertseva Eva <12gerts@bk.ru>
2022-10-17 14:37:48 +0500

    Update LogicTest.java

--- src/test/java/LogicTest.java
+++ src/test/java/LogicTest.java
@@ -1,39 +1,58 @@
 import org.bot.*;
-import org.bot.Http.HttpRequest;
-import org.bot.Reader;
 import org.bot.Telegram.Telegram;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
-import java.io.*;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.io.IOException;
+import java.util.Objects;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-class LogicTest {
-    Logic logic = new Logic();
-    Week week = new Week();
-    Group group = new Group();
-    Reader reader = new Reader();
-    HttpRequest request = new HttpRequest();
-    String fixDay = "11.10.2022";
-    Date fixDate = new SimpleDateFormat("dd.MM.yyyy").parse(fixDay);
+public class LogicTest {
 
-    LogicTest() throws ParseException {
+    private final Logic logic = new Logic(
+            new GroupTest.MockHttpRequest(),
+            new MockWeek(),
+            new MockGroup()
+    );
+    private static final Reader reader = new Reader();
+    private static final String today;
+
+    static {
+        try {
+            today = reader.readFile("src/test/resources/today.txt");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
+    private static final String tomorrow;
 
-    @BeforeAll
-    public static void putMap() {
-        Telegram.map.put("1", null);
-        Telegram.map.put("2", null);
-        Telegram.map.put("3", "54627");
-        Telegram.map.put("4", null);
-        Telegram.map.put("5", null);
-        Telegram.map.put("6", null);
-        Telegram.map.put("7", null);
+    static {
+        try {
+            tomorrow = reader.readFile("src/test/resources/tomorrow.txt");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static final String week;
+
+    static {
+        try {
+            week = reader.readFile("src/test/resources/week.txt");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static final String weeks;
+
+    static {
+        try {
+            weeks = reader.readFile("src/test/resources/weeks.txt");
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 
     @Test
@@ -50,181 +69,161 @@ class LogicTest {
         assertEquals(expected, actual);
     }
 
-
     @Test
-    void getReportDefault() {
-        String actual = logic.parseMessage("/non-exist", null);
-        String expected = Report.DEFAULT_REPORT;
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void getInnerNumber() {
-        String actual = group.convertAndUpdateNumberOfGroup("мен-210204", "1");
-        String expected = "54627";
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void getIncorrectInnerNumber() {
-        String actual = group.convertAndUpdateNumberOfGroup("мен-000000", "1");
-        assertNull(actual);
-    }
-
-    @Test
-    void repeatTodayCommand() {
-        logic.parseMessage("/today", "2");
-        String actual = logic.parseMessage("/today", "2");
-        String expected = Report.AUTHORIZATION_REPORT;
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void getCalendar() {
-        String actual = request.getSchedule("54627").substring(0, 15);
-        String expected = "BEGIN:VCALENDAR";
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void getToday() throws IOException {
-        week.today = fixDate;
-        String calendar = reader.readFile("src/test/calendar.ics");
-        String expected = reader.readFile("src/test/today.txt");
-        String actual = week.today(calendar);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void getIllegalStateException() {
-        assertThrows(IllegalStateException.class, () -> week.nameOfDay(8));
-    }
-
-
-    @Test
-    void getTomorrow() throws IOException {
-        week.today = fixDate;
-        String calendar = reader.readFile("src/test/calendar.ics");
-        String expected = reader.readFile("src/test/tomorrow.txt");
-        String actual = week.tomorrow(calendar);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void getWeek() throws IOException {
-        week.today = fixDate;
-        String calendar = reader.readFile("src/test/calendar.ics");
-        String expected = reader.readFile("src/test/week.txt");
-        String actual = week.week(calendar, 7);
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void getWeeks() throws IOException {
-        week.today = fixDate;
-        String calendar = reader.readFile("src/test/calendar.ics");
-        String expected = reader.readFile("src/test/weeks.txt");
-        String actual = week.week(calendar, 14);
-        assertEquals(expected, actual);
-    }
-
-
-    @Test
-    void readFile() throws IOException {
-        String expected = "Test 1\nTest 2";
-        String actual = reader.readFile("src/test/test_reader.txt");
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void getBotUsername() {
-        Telegram telegram = new Telegram();
-        String expected = telegram.getBotUsername();
-        assertNotEquals(null, expected);
-    }
-
-    @Test
-    void getBotToken() {
-        Telegram telegram = new Telegram();
-        String expected = telegram.getBotToken();
-        assertNotEquals(null, expected);
-    }
-
-    @Test
-    void getChange() {
+    void getReportChange() {
         String actual = logic.parseMessage("/change", null);
         String expected = Report.AUTHORIZATION_REPORT;
         assertEquals(expected, actual);
     }
 
     @Test
-    void getIncorrectLastChange() {
-        logic.parseMessage("/change", "1");
-        String actual = logic.parseMessage("1234567890", "1");
-        String expected = Report.REQUEST_ERROR;
-        assertEquals(expected, actual);
-    }
+    void getCorrectChange() {
+        Telegram.map.put("1", "123");
+        logic.setLastMessage("/change");
 
-    @Test
-    void getLastChange() {
-        logic.parseMessage("/change", "1");
-        String actual = logic.parseMessage("мен-210201", "1");
+        String actual = logic.parseMessage("мен-210204", "1");
         String expected = Report.GROUP_CHANGE;
         assertEquals(expected, actual);
     }
 
     @Test
-    void getLastToday() {
-        logic.parseMessage("/today", "4");
-        String schedule = logic.parseMessage("мен-210201", "4");
-        int index = schedule.indexOf("\n");
-        String actual = schedule.substring(0, index);
-        String expected = week.nameOfDay(week.getNumberOfWeekDay(new Date()));
-        assertEquals(expected, actual);
-    }
+    void getIncorrectChange() {
+        Telegram.map.put("2", "123");
+        logic.setLastMessage("/change");
 
-    @Test
-    void getLastTomorrow() {
-        logic.parseMessage("/tomorrow", "2");
-        String schedule = logic.parseMessage("мен-210201", "2");
-        int index = schedule.indexOf("\n");
-        String actual = schedule.substring(0, index);
-        String expected = week.nameOfDay(week.getNumberOfWeekDay(week.getNextDay(new Date())));
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void getLastWeek() {
-        logic.parseMessage("/week", "5");
-        String schedule = logic.parseMessage("мен-210201", "5");
-        int index = schedule.indexOf("\n");
-        String actual = schedule.substring(0, index);
-        String expected = week.nameOfDay(week.getNumberOfWeekDay(new Date()));
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void getLastWeeks() {
-        logic.parseMessage("/weeks", "6");
-        String schedule = logic.parseMessage("мен-210201", "6");
-        int index = schedule.indexOf("\n");
-        String actual = schedule.substring(0, index);
-        String expected = week.nameOfDay(week.getNumberOfWeekDay(new Date()));
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    void getWrongGroup() {
-        logic.parseMessage("/weeks", "7");
-        String actual = logic.parseMessage("мен-000000", "7");
+        String actual = logic.parseMessage("мен-000000", "2");
         String expected = Report.REQUEST_ERROR;
         assertEquals(expected, actual);
     }
 
     @Test
-    void getWrongHttpRequest() {
-        String actual = request.getSchedule("00000000000");
-        assertNull(actual);
+    void getDoubleCommand() {
+        Telegram.map.put("1", null);
+        logic.setLastMessage("/today");
+
+        String actual = logic.parseMessage("/today", "1");
+        String expected = Report.AUTHORIZATION_REPORT;
+        assertEquals(expected, actual);
     }
 
+    @Test
+    void addGroupAndGetScheduleWhenCorrect() {
+        Telegram.map.put("1", null);
+        logic.setLastMessage("/today");
+
+        String actual = logic.parseMessage("мен-210204", "1");
+        assertEquals(today, actual);
+    }
+
+    @Test
+    void addGroupAndGetScheduleWhenIncorrect() {
+        Telegram.map.put("1", null);
+        logic.setLastMessage("/today");
+
+        String actual = logic.parseMessage("мен-000000", "1");
+        String expected = Report.REQUEST_ERROR;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getReportDefault() {
+        String actual = logic.getReport("/non-exist", null);
+        String expected = Report.DEFAULT_REPORT;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getAuthorizationReportWhenTodayAndNull() {
+        String actual = logic.getReport("/today", null);
+        String expected = Report.AUTHORIZATION_REPORT;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getAuthorizationReportWhenTomorrowAndNull() {
+        String actual = logic.getReport("/tomorrow", null);
+        String expected = Report.AUTHORIZATION_REPORT;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getAuthorizationReportWhenWeekAndNull() {
+        String actual = logic.getReport("/week", null);
+        String expected = Report.AUTHORIZATION_REPORT;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getAuthorizationReportWhenWeeksAndNull() {
+        String actual = logic.getReport("/weeks", null);
+        String expected = Report.AUTHORIZATION_REPORT;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void getAuthorizationReportWhenTodayAndNotNull() {
+        String actual = logic.getReport("/today", "1");
+        assertEquals(today, actual);
+    }
+
+    @Test
+    void getAuthorizationReportWhenTomorrowAndNotNull() {
+        String actual = logic.getReport("/tomorrow", "1");
+        assertEquals(tomorrow, actual);
+    }
+
+    @Test
+    void getAuthorizationReportWhenWeekAndNotNull() {
+        String actual = logic.getReport("/week", "1");
+        assertEquals(week, actual);
+    }
+
+    @Test
+    void getAuthorizationReportWhenWeeksAndNotNull() {
+        String actual = logic.getReport("/weeks", "1");
+        assertEquals(weeks, actual);
+    }
+
+    public static class MockWeek implements IWeek {
+
+        @Override
+        public String today(String calendar) {
+            return today;
+        }
+
+        @Override
+        public String tomorrow(String calendar) {
+            return tomorrow;
+        }
+
+        @Override
+        public String week(String calendar, int amountOfDays) {
+            if (amountOfDays == 7) {
+                return week;
+            } else if (amountOfDays == 14) {
+                return weeks;
+            }
+            return null;
+        }
+    }
+    public static class MockGroup implements IGroup {
+        @Override
+        public String convertAndUpdateNumberOfGroup(String group, String chatId) {
+            if (Objects.equals(group, "мен-210204")) {
+                Telegram.map.replace(chatId, "54627");
+            } else {
+                Telegram.map.replace(chatId, null);
+            }
+            return null;
+        }
+
+        @Override
+        public String checkGroupChange(String chatId) {
+            if (Objects.equals(chatId, "1")) {
+                return Report.GROUP_CHANGE;
+            } else {
+                return Report.REQUEST_ERROR;
+            }
+        }
+    }
 }
